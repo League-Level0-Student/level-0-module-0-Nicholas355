dcf8dc2ed7fe71996245950c5f44bc59fa92566d
root <root@306ce816fd82>
2020-12-05 19:29:03 +0000

    The Ninja's Star

--- src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
+++ src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
@@ -11,6 +11,7 @@ public class FlamingNinjaStar {
 		
 		// Make a new robot, and set it's pen down.
 		Robot Nick = new Robot();
+		Nick.penDown();
 		// Set the robot speed to 100
 		Nick.setSpeed(100);
 		// Set the robot window size to 800 x 800
@@ -19,25 +20,28 @@ public class FlamingNinjaStar {
 		//         Set its start value to zero.
 		int count =0;
 	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
-		while(count < 1) {
+		while(count < 25) {
+			Nick.setPenColor(Color.black);
 			   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
 		       Nick.turn(360/8);
 			   // MOVE           Move the robot 64 pixels
 		       Nick.move(64);
 			   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			   Nick.turn(-40/8);
+			   Nick.turn(-40);
 			   // DRAW FLAME     Move the robot the distance in the variable flameSize
-			
-				//               Turn the robot 170 degrees
-			
-				//               Move the robot the distance in the variable flameSize (again)
-			
+			   Nick.setPenColor(Color.red);
+			   Nick.move(flameSize);
+			   //               Turn the robot 170 degrees
+			   Nick.turn(170);
+			   //               Move the robot the distance in the variable flameSize (again)
+			   Nick.move(flameSize);
+			   Nick.setPenColor(Color.black);
 			   // TURN RIGHT     Turn the robot 64 degrees to the right
-			
-				// MOVE         Move the robot the distance in the variable baseSize
-		
-				//  INCREASE COUNT. Increase the count by 1
-		
+			   Nick.turn(64);
+			   // MOVE         Move the robot the distance in the variable baseSize
+		       Nick.move(baseSize);
+		       //  INCREASE COUNT. Increase the count by 1
+		       count++;
 		//  End the while loop here
 		}
 		// TEST   Run the program. Check that your shape is the same as the first picture in the recipe. 
@@ -45,7 +49,7 @@ public class FlamingNinjaStar {
 		
 		// COLOR  Change the Robot's pen color so that the flame is a different color to the rest of the star.
 		//        Run the program again. Check the second picture in the recipe.
-
+		
 		// LOOP   When you have one arm looking right, change your loop to repeat 25 times and run the program.
 		
 	}
